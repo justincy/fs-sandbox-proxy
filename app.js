c535a0c53d64340b1c23aa90a8509c619c72dfb3
Justin York <justincyork@gmail.com>
2013-01-30 20:54:16 -0700

    Add 'link' to the expose header

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(req, res){
   res.setHeader('Access-Control-Allow-Origin', 'https://familysearch.org');
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'authorization, content-type, x-reason');
-  res.setHeader('Access-Control-Expose-Headers', 'location');
+  res.setHeader('Access-Control-Expose-Headers', 'location, link');
 
   // Return CORS headers when OPTIONS method is received
   if( req.method === 'OPTIONS' ) {
